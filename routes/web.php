ae5c66b8566cf2ccb5e24cdaf808013867eb1319
valentin <valentinscramin@gmail.com>
2022-08-11 16:59:03 -0300

    paginas: profile, van, site e pagina de login admin e usuario

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//ROTAS SITE
 Route::get('/', function () {
-    return view('welcome');
+    return view('site.home');
 });
 
 Auth::routes();
 
 //USER
-Route::get('/user', [App\Http\Controllers\User\HomeController::class, 'index'])->name('user_home');
+Route::get('/user', [App\Http\Controllers\User\HomeController::class, 'index'])->name('user_home')->middleware('auth');
+Route::resource('/user/profile', App\Http\Controllers\User\ProfileController::class)->middleware('auth');
+Route::resource('/user/van', App\Http\Controllers\User\VanController::class)->middleware('auth');
 
 //ADMIN
-Route::get('/admin', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin_home');
+Route::get('/admin', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin_home')->middleware('auth:admin');
 Route::get('/admin/login', [App\Http\Controllers\Auth\LoginAdminController::class, 'index'])->name('admin_login');
 Route::post('/admin/login', [App\Http\Controllers\Auth\LoginAdminController::class, 'login'])->name('admin_login_submit');
