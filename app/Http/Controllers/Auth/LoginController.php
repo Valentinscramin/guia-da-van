f177bac6dfe82526ceb7043bdb78fa26c47c5f77
valentin <valentinscramin@gmail.com>
2022-08-15 20:55:04 -0300

    correcao de redirecionamento ao logar na aplicacao

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/user';
+    protected $redirectTo = RouteServiceProvider::HOME_USER;
 
     /**
      * Create a new controller instance.
