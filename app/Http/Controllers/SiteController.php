71076c6f36e58e3380282c34bfc2a78ad87a18bd
valentin <valentinscramin@gmail.com>
2022-09-02 17:01:15 -0300

    switch case busca

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -21,6 +21,29 @@ class SiteController extends Controller
     public function busca(Request $request)
     {
         //dump($request->cidade_chegada_escola);
+        
+        switch ($request->track) {
+            case 1:
+                $request->cidade_saida_escola;
+                $request->cidade_chegada_escola;
+                $request->escola;
+                $request->periodo;
+            break;
+
+            case 2:
+                $request->cidade_saida_evento;
+                $request->evento;
+            break;
+            
+            case 3:
+                $request->cidade_saida_executivo;
+            break;
+
+            case 4:
+                $request->cidade_saida_frete;
+            break;
+        }
+        
         return view('site.resultado', compact('vans'));
     }
 
