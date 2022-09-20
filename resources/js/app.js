4471e26822e67e56573efbd1b318102f156d9dd2
gui-goncales <91754684+gui-goncales@users.noreply.github.com>
2022-09-20 01:24:47 -0300

    update filtro

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@ import $ from 'jquery';
 window.$ = window.jQuery = $;
 
 $(function () {
+    $(".search .formulario .item:nth-child(3) .item_input input").attr('checked', true)
+    $(".collapse_filter #content_track-1").addClass("activeFilter")
     $(".search .formulario .item .item_input input").click(function(){
         var id = $(this).attr("id")
         $(".search .formulario .item .item_input input").attr('checked', false)
