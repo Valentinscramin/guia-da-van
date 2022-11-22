71c06cecc1438ba4bd146855dbb93b5513b1b105
gui-goncales <91754684+gui-goncales@users.noreply.github.com>
2022-11-21 21:26:12 -0300

    update result page - Valentin cheira boga

--- resources/views/site/resultado.blade.php
+++ resources/views/site/resultado.blade.php
@@ -1,63 +1,56 @@
 @extends('layouts.appSite')
 
 @section('content')
-    <section>
-        <div class="bannerTop">
-            <div class="container">
-                <div class="row no-gutters">
-                    <div class="col-12 col-lg-8">
-                        <div class="takeItem">
-                            <div class="button_tag">
-                                <h2>O que fazemos?</h2>
-                            </div>
-
-                            <div class="title">
-                                <h2>A procura por vans de <span>forma simples e eficaz</span></h2>
-                            </div>
-
-                            <div class="description col-12 col-md-10 col-lg-9">Guia da Van é um sistema desenvolvido para
-                                facilitar a oferta e procura por serviços com Vans através do cadastro pelos próprios
-                                proprietários de uma forma muito simples.</div>
+<section>
+    <div class="bannerTop">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-12 col-lg-8">
+                    <div class="takeItem">
+                        <div class="button_tag">
+                            <h2>Resultado de busca</h2>
                         </div>
-                        <div class="search">
-                            <div class="titleSearch">
-                                <h2>Busca Personalizada</h2>
-                            </div>
-                            <div class="formulario col-12 col-md-10">
-                                @include('site.filtro')
-                            </div>
+
+                        <div class="title">
+                            <h2>A procura por vans de <span>forma simples e eficaz</span></h2>
                         </div>
                     </div>
-                    <div class="col-12 col-lg-4">
-                        <div class="imagem" style="position: absolute; right: 0; text-align: right; z-index: 2;"></div>
+                    <div class="search">
+                        <div class="titleSearch">
+                            <h2>Busca Personalizada</h2>
+                        </div>
+                        <div class="formulario col-12 col-md-10">
+                            @include('site.filtro')
+                        </div>
                     </div>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <div class="imagem" style="position: absolute; right: 0; text-align: right; z-index: 2;"></div>
                 </div>
             </div>
         </div>
-    </section>
-    <div class="container">
-        <div class="all_cards_result">
-            @foreach ($vans as $eachVan)
-                <div class="card_item text-start col-12 col-sm-6 col-lg-4 col-xl-3">
-                    <div class="takeCard">
-                        <img class="card-img-top"
-                            src="https://cdn.cloudflare.steamstatic.com/steam/apps/1548130/capsule_616x353.jpg?t=1631134025"
-                            alt="Title">
-                        <div class="card-body">
-                            <h4 class="card-title">{{ $eachVan->van }}</h4>
-                            <p class="card-text">Placa: {{ $eachVan->placa }}</p>
-                            <p class="card-text">Usuario: <a href="{{ route('profile_show', $eachVan->usuario_id) }}"
-                                    target="_blank">{{ $eachVan->usuario_nome }}</a></p>
-                            <p class="card-text">Celular: {{ $eachVan->usuario_celular }}</p>
-                            <p class="card-text">Trajeto: {{ $eachVan->trajeto }}</p>
-                            @if (!is_null($eachVan->periodo))
-                                <p class="card-text">Periodo: {{ $eachVan->periodo }}</p>
-                            @endif
-                            <p class="card-text">observacoes: {{ $eachVan->comment }}</p>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
     </div>
+</section>
+<div class="container">
+    <div class="all_cards_result">
+        @foreach ($vans as $eachVan)
+        <div class="card_item text-start col-12 col-sm-6 col-lg-4 col-xl-3">
+            <div class="takeCard">
+                <img class="card-img-top" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1548130/capsule_616x353.jpg?t=1631134025" alt="Title">
+                <div class="card-body">
+                    <h4 class="card-title">{{ $eachVan->van }}</h4>
+                    <p class="card-text">Placa: {{ $eachVan->placa }}</p>
+                    <p class="card-text">Usuario: <a href="{{ route('profile_show', $eachVan->usuario_id) }}" target="_blank">{{ $eachVan->usuario_nome }}</a></p>
+                    <p class="card-text">Celular: {{ $eachVan->usuario_celular }}</p>
+                    <p class="card-text">Trajeto: {{ $eachVan->trajeto }}</p>
+                    @if (!is_null($eachVan->periodo))
+                    <p class="card-text">Periodo: {{ $eachVan->periodo }}</p>
+                    @endif
+                    <p class="card-text">observacoes: {{ $eachVan->comment }}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
