901aeccec3aa508cc26a1d73e2fa27f016aa1c97
valentin <valentinscramin@gmail.com>
2022-11-21 21:09:55 -0300

    ok

--- resources/views/site/resultado.blade.php
+++ resources/views/site/resultado.blade.php
@@ -35,25 +35,29 @@
             </div>
         </div>
     </section>
-    <div class="container" style="display:flex;">
-        @foreach ($vans as $eachVan)
-            <div class="card text-start">
-                <img class="card-img-top"
-                    src="https://cdn.cloudflare.steamstatic.com/steam/apps/1548130/capsule_616x353.jpg?t=1631134025"
-                    alt="Title">
-                <div class="card-body">
-                    <h4 class="card-title">{{ $eachVan->van }}</h4>
-                    <p class="card-text">Placa: {{ $eachVan->placa }}</p>
-                    <p class="card-text">Usuario: <a href="{{ route('profile_show', $eachVan->usuario_id) }}"
-                            target="_blank">{{ $eachVan->usuario_nome }}</a></p>
-                    <p class="card-text">Celular: {{ $eachVan->usuario_celular }}</p>
-                    <p class="card-text">Trajeto: {{ $eachVan->trajeto }}</p>
-                    @if (!is_null($eachVan->periodo))
-                        <p class="card-text">Periodo: {{ $eachVan->periodo }}</p>
-                    @endif
-                    <p class="card-text">observacoes: {{ $eachVan->comment }}</p>
+    <div class="container">
+        <div class="all_cards_result">
+            @foreach ($vans as $eachVan)
+                <div class="card_item text-start col-12 col-sm-6 col-lg-4 col-xl-3">
+                    <div class="takeCard">
+                        <img class="card-img-top"
+                            src="https://cdn.cloudflare.steamstatic.com/steam/apps/1548130/capsule_616x353.jpg?t=1631134025"
+                            alt="Title">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $eachVan->van }}</h4>
+                            <p class="card-text">Placa: {{ $eachVan->placa }}</p>
+                            <p class="card-text">Usuario: <a href="{{ route('profile_show', $eachVan->usuario_id) }}"
+                                    target="_blank">{{ $eachVan->usuario_nome }}</a></p>
+                            <p class="card-text">Celular: {{ $eachVan->usuario_celular }}</p>
+                            <p class="card-text">Trajeto: {{ $eachVan->trajeto }}</p>
+                            @if (!is_null($eachVan->periodo))
+                                <p class="card-text">Periodo: {{ $eachVan->periodo }}</p>
+                            @endif
+                            <p class="card-text">observacoes: {{ $eachVan->comment }}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 @endsection
