f4a7aa7159eadc98c0608f2d35d9c029a4a8f148
valentin <valentinscramin@gmail.com>
2022-12-06 21:38:41 -0300

    correcao envio de email

---
+++ resources/views/auth/contact.blade.php
@@ -0,0 +1,3 @@
+Enviado por: {{ $name }}
+<p> Email: {{ $email }} </p>
+<p> Mensagem: {{ $comment }} </p>
--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -33,28 +33,34 @@
             <div class="container">
                 <div class="row no-gutters">
                     <div class="formulario col-12 col-lg-10 col-xl-8" style="margin: 0 auto;">
-                        <form method="POST" action="#">
+                        <form method="POST" action="{{ route('send_email') }}">
                             @csrf
                             @method('POST')
                             <input type="hidden" name="view" value="contato">
                             <div class="itemInput col-12 col-sm-6">
                                 <label for="name" class="form-label">Nome</label>
-                                <input type="text" name="name" id="name" placeholder="" aria-describedby="helpId"
-                                    value="">
+                                <input type="text" name="name" id="name" placeholder="" aria-describedby="helpId">
                             </div>
 
                             <div class="itemInput col-12 col-sm-6">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="text" name="name" id="email" placeholder="" aria-describedby="helpId"
-                                    value="">
+                                <input type="email" name="email" id="email" placeholder="" aria-describedby="helpId">
                             </div>
 
                             <div class="itemInput col-12 col-sm-12">
-                                <label for="message" class="form-label">Mensagem</label>
-                                <textarea name="message" class="form-control"></textarea>
+                                <label for="comment" class="form-label">Mensagem</label>
+                                <textarea name="comment" class="form-control"></textarea>
                             </div>
                             <div class="col-12 btn_submit"><button type="submit">Enviar</button></div>
                         </form>
+
+                        @if (session('success'))
+                            <br>
+                            <br>
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
